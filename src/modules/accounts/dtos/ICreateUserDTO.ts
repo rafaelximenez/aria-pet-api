0030e466cb08a5f74e286134132cdbd746e0da30
Rafael <rximenez97@gmail.com>
2023-01-08 20:00:18 -0300

    refact: implement auth jwt

--- src/modules/accounts/dtos/ICreateUserDTO.ts
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -1,8 +1,9 @@
 interface ICreateUserDTO{
     name: string;
-    username: string;
     email: string;
     password: string;
+    id?: string;
+    avatar?: string;
 }
 
 export { ICreateUserDTO };
