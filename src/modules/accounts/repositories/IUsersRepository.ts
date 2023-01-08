0e280601ec936f4c3639336dddf64e6bcfc20930
Rafael <rximenez97@gmail.com>
2023-01-08 19:59:53 -0300

    feat: upload avatar

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -3,6 +3,8 @@ import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
 
 interface IUsersRepository{
     create(data: ICreateUserDTO): Promise<void>;
+    findByEmail(email:string): Promise<User>;
+    findById(id:string): Promise<User>;
 }
 
 export { IUsersRepository }
---
+++ src/utils/file.ts
@@ -0,0 +1,9 @@
+import fs from "fs";
+export const deleteFile = async(filename: string) => {
+    try{
+        await fs.promises.stat(filename);
+    }catch{
+        return;
+    }
+    await fs.promises.unlink(filename);
+}
